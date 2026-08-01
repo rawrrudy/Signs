59debe635418cbf259052e189315d7c5bc9a1fc4
Rudra Sundgikar <rudrasundgikar@gmail.com>
2026-08-01 21:15:18 +0530

    Switched values in boot scene

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -5,9 +5,40 @@ export default class BootScene extends Phaser.Scene {
     super("BootScene");
   }
   preload() {
-    console.log("Loading assets...");
+
+    this.load.image("road", "src/assets/road.png");
+    this.load.image("fireRoad", "src/assets/fireroad.png");
+    this.load.image("brokenRoad", "src/assets/brokenroad.png");
+    this.load.image("camera", "src/assets/camera.png");
+    this.load.image("caution", "src/assets/cautionsign.png");
+    this.load.image("run1", "src/assets/runframe1.png");
+    this.load.image("run2", "src/assets/runframe2.png");
+    this.load.image("run3", "src/assets/runframe3.png");
+    this.load.image("run4", "src/assets/runframe4.png");
+    this.load.image("run5", "src/assets/runframe5.png");
+
   }
   create() {
+    this.anims.create({
+
+        key: "run",
+
+        frames: [
+            { key: "run1" },
+            { key: "run2" },
+            { key: "run3" },
+            { key: "run4" },
+            { key: "run5" },
+        ],
+
+        frameRate: 10,
+
+        repeat: -1,
+ 
+    });
     this.scene.start("MenuScene");
   }
 }
+
+
+
