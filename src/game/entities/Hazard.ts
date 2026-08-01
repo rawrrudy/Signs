f425265a5abcb0e298607462cfd5179dd486a5cd
Rudra Sundgikar <rudrasundgikar@gmail.com>
2026-08-01 20:48:07 +0530

    Added a threshold limit to danger

--- src/game/entities/Hazard.ts
+++ src/game/entities/Hazard.ts
@@ -1,9 +1,12 @@
 import Phaser from "phaser";
 
 export default class Hazard extends Phaser.GameObjects.Ellipse {
-  constructor(scene: Phaser.Scene, x: number, y: number) {
-    super(scene, x, y, 80, 50, 0x3da9fc);
 
-    scene.add.existing(this);
-  }
+    constructor(scene: Phaser.Scene,x:number,y:number){
+        super(scene,x,y,120,70,0x4aa3ff);
+        scene.add.existing(this);
+        scene.add.text(x,y-70,"💧",{
+            fontSize:"32px"
+        }).setOrigin(0.5);
+    }
 }
--- src/game/scenes/GameScene.ts
+++ src/game/scenes/GameScene.ts
@@ -6,17 +6,41 @@ export default class GameScene extends Phaser.Scene {
 
     private npc!: NPC;
 
-    create() {
-
-        this.cameras.main.setBackgroundColor("#dcdcdc");
-
-        this.npc = new NPC(this,100,360);
-
-        new Hazard(this,900,360);
-
+    constructor() {
+        super("GameScene");
     }
 
-    update(_:number,delta:number){
+    create() {
+
+      this.cameras.main.setBackgroundColor("#E8E6E3");
+
+      // ground
+      this.add.rectangle(640,360,1280,720,0xe8e6e3);
+
+      // road
+      this.add.rectangle(640,360,1280,140,0x7d7d7d);
+
+      // footpath
+      this.add.rectangle(640,260,1280,40,0xcfcfcf);
+
+      // hazard
+      new Hazard(this,950,360);
+
+      this.add.line(
+        0,
+        0,
+        900,
+        320,
+        900,
+        400,
+        0xff0000
+      ).setLineWidth(4);
+
+      // NPC
+      this.npc = new NPC(this,120,360);
+  }
+
+    update(_: number, delta: number) {
 
         this.npc.update(delta);
 
